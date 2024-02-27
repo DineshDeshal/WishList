55eef8538390f440380b65f1751ccd63e90bd946
DineshDeshal <sharmadinesh1404@gmail.com>
2024-02-27 19:39:34 +0530

    ok

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -13,11 +13,16 @@ function UserInput() {
         setWish(""); 
     }
 
+    function deleteAll() {
+        setWishes([]); 
+        // setWishes(["ohh sorry now you have to need first add any wish😮"])
+    }
+
     return (
         <form>
             <div id='firstwish'>
                 <label htmlFor="wish"> Wish :- </label>
-                <input type="text" id='wish' value={wish} onChange={handleInputChange} />
+                <input type="text" id='wish' value={wish} onChange={handleInputChange} placeholder='please write your wish here' />
                 <button type="button" onClick={handleAdd}>ADD</button>
                 
                 <ul>
@@ -25,9 +30,12 @@ function UserInput() {
                         <li key={index}>{item}</li>
                     ))}
                 </ul>
+                <button type="button" onClick={deleteAll}>Clear All</button>
+               
             </div>
         </form>
     );
 }
 
 export default UserInput;
+
